1b073dbb27ba767aca105c7359e4eed55fdc9b2b
RAO NISAR AHMED <MR-NR@users.noreply.github.com>
2018-12-21 18:26:23 +0500

    Include project

---
+++ DataBaseFirstDemo/DataBaseFirstDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DataBaseFirstDemoEntities();
+
+
+            var student = new Student()
+            {
+                Name = "Nisar Ahmed",
+                Programe ="BS(CS)",
+                Date = DateTime.Now,
+                StudentId = 1
+            };
+
+            context.Students.Add(student);
+
+       
+            context.SaveChanges();
+            
+        }
+    }
+}
